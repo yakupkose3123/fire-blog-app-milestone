6524c67f5ff06404d80cc97b9e36ae504a022ec5
yakupkose3123 <yakupkose3123@gmail.com>
2022-04-27 16:40:20 +0300

    created project skeleton

---
+++ src/app-router/PrivateRouter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PrivateRouter = () => {
+  return (
+    <div>
+      <h1>PrivateRouter</h1>
+    </div>
+  )
+}
+
+export default PrivateRouter
---
+++ src/components/BlogCars.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BlogCars = () => {
+  return (
+    <div>BlogCars</div>
+  )
+}
+
+export default BlogCars
